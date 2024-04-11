be4e18ea48dd62c0ba7eb5958687426377cd14e4
Rodolpho Reis <rodolphoreis@live.com>
2024-04-11 17:56:10 +0100

    chore: refactor

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Logo() {
   return (
-    <div className="logo">
-      <img src="/logo.jpeg" alt="logo" />
+    <div className="container">
+      <div className="logo">
+        <img src="/logo.jpeg" alt="Logo" />
+      </div>
     </div>
   );
 }

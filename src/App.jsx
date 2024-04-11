49bd4583733684ceabf1c2a153a233e3855c13e2
Rodolpho Reis <rodolphoreis@live.com>
2024-04-11 17:24:30 +0100

    feat: create app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "./components/Logo";
+import Mains from "./components/Mains";
+import Extras from "./components/Extras";
+import { Provider } from "./Context";
+import { pratos, acompanhamentos, bebidas } from "./data";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Provider>
+      <div className="menu">
+        <Logo />
+        <Mains meals={pratos} />
+        <aside className="aside">
+          <Extras type="Acompanhamentos" items={acompanhamentos} />
+          <Extras type="Bebidas" items={bebidas} />
+        </aside>
+      </div>
+    </Provider>
+  );
+}

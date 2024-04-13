e6cdbc9031e56ff4f999d6925720a1c52329b2e4
Rodolpho Reis <rodolphoreis@live.com>
2024-04-13 21:53:04 +0100

    feat: add component BackButton

--- src/components/Extras.jsx
+++ src/components/Extras.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Extras({ type, items }) {
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <section className="extras">
-      <h2 className="extras-heading">{type}</h2>
-      {items.map((item, index) => (
-        <article className="menu-item" key={index}>
-          <div className="extras-name">{item.nome}</div>
-          <strong className="extras-price">€{item.preço}</strong>
-        </article>
-      ))}
-    </section>
+    <button onClick={handleGoBack} className="button_voltar">
+      Voltar
+    </button>
   );
-}
+};
+
+const Extras = ({ type, items }) => {
+  return (
+    <div>
+      <section className="extras">
+        <h2 className="extras-heading">{type}</h2>
+        {items.map((item, index) => (
+          <article className="menu-item" key={index}>
+            <div className="extras-name">{item.nome}</div>
+            <strong className="extras-price">€{item.preço}</strong>
+          </article>
+        ))}
+      </section>
+      <BackButton />
+    </div>
+  );
+};
+
+export default Extras;

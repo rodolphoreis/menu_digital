933828bc00e1384736d84f4f70995b7f1776ea7f
Rodolpho Reis <rodolphoreis@live.com>
2024-04-13 21:22:28 +0100

    refactor: create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Logo from "./components/Logo";
+import Home from "./components/Home";
 import Mains from "./components/Mains";
 import Extras from "./components/Extras";
+import Drink from "./components/Drink";
 import { Provider } from "./Context";
 import { pratos, acompanhamentos, bebidas } from "./data";
-
 import "./styles.css";
 
 export default function App() {
   return (
     <Provider>
-      <Logo />
-      <div className="menu">
-        <Mains meals={pratos} />
-        <aside className="aside">
-          <Extras type="Acompanhamentos" items={acompanhamentos} />
-          <Extras type="Bebidas" items={bebidas} />
-        </aside>
-      </div>
+      <Router>
+        <Logo />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mains" element={<Mains meals={pratos} />} />
+          <Route
+            path="/extras"
+            element={<Extras type="Acompanhamentos" items={acompanhamentos} />}
+          />
+          <Route
+            path="/drink"
+            element={<Drink type="Bebidas" items={bebidas} />}
+          />
+        </Routes>
+      </Router>
     </Provider>
   );
 }

f6f2b13e0d049ff58e6b5f1a90567fa37a976e12
Rodolpho Reis <rodolphoreis@live.com>
2024-04-14 19:57:11 +0100

    feat: create component

---
+++ src/components/Desserts.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button onClick={handleGoBack} className="button_voltar">
+      Voltar
+    </button>
+  );
+};
+
+const Desserts = ({ type, items }) => {
+  return (
+    <div>
+      <section className="extras">
+        <h2 className="extras-heading">{type}</h2>
+        {items.map((item, index) => (
+          <article className="menu-item" key={index}>
+            <div className="extras-name">{item.nome}</div>
+            <strong className="extras-price">€{item.preço}</strong>
+          </article>
+        ))}
+      </section>
+      <div className="BackButton">
+        <BackButton />
+      </div>
+    </div>
+  );
+};
+
+export default Desserts;

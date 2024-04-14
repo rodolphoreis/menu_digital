ac39166c99a05f196481d6dc9c32a951867f6b99
Rodolpho Reis <rodolphoreis@live.com>
2024-04-14 18:25:18 +0100

    chore: refactor component

--- src/components/Mains.jsx
+++ src/components/Mains.jsx
@@ -25,33 +25,43 @@ export default function Mains({ meals }) {
       imageAlt: meal.nome,
       showCloseButton: false,
       showCancelButton: false,
-      confirmButtonColor: "#4a4a4a",
+      confirmButtonColor: "#a37443",
       confirmButtonText: "Fechar",
       confirmButtonClass: "custom-close-button",
       focusConfirm: false,
+      didRender: () => {
+        const confirmButton = document.querySelector(".swal2-confirm");
+        if (confirmButton) {
+          confirmButton.style.border = "none";
+        }
+      },
       width: "280px",
       height: "auto",
     });
   };
 
   return (
-    <section className="mains">
-      {meals.map((meal, index) => (
-        <article className="menu-item" key={index}>
-          <div className="meal-info">
-            <h3 className="mains-name">{meal.nome}</h3>
-            <button
-              className="ver-imagem-button"
-              onClick={() => openModal(meal)}
-            >
-              Ver Imagem
-            </button>
-          </div>
-          <strong className="mains-price">€{meal.preço}</strong>
-          <p className="mains-description">{meal.descrição}</p>
-        </article>
-      ))}
-      <BackButton />
-    </section>
+    <>
+      <section className="mains">
+        {meals.map((meal, index) => (
+          <article className="menu-item" key={index}>
+            <div className="meal-info">
+              <h3 className="mains-name">{meal.nome}</h3>
+              <button
+                className="ver-imagem-button"
+                onClick={() => openModal(meal)}
+              >
+                Ver Imagem
+              </button>
+            </div>
+            <strong className="mains-price">€{meal.preço}</strong>
+            <p className="mains-description">{meal.descrição}</p>
+          </article>
+        ))}
+      </section>
+      <div className="BackButton">
+        <BackButton />
+      </div>
+    </>
   );
 }
